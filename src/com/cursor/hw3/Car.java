119c6f036404f6d9ff2e5be3c6e4f1570ec603a9
Mila <milkevich.mila@gmail.com>
2019-12-09 15:39:01 +0200

    4 file was added

---
+++ src/com/cursor/hw3/Car.java
@@ -0,0 +1,42 @@
+package com.cursor.hw3;
+
+public class Car {
+    private String model;
+    private boolean start;
+    private double speed;
+
+    public void startMotor() {
+        if (start) {
+            System.out.println(model + " " + "started up.");
+        } else {
+            System.out.println("Motor is muffled.");
+        }
+    }
+
+    public void drive() {
+        if (start) {
+            if (speed > 0) {
+                System.out.println(model + " " + "rides.");
+            }
+        } else {
+            System.out.println(model + " " + "is standing.");
+        }
+    }
+
+    public void speed() {
+        if (start && speed > 50) {
+            System.out.println("Over speed.");
+        } else if (start && speed <= 50) {
+            System.out.println(model + " " + "moves with speed " + " " + speed + " " + " kilometers per hour.");
+        }
+    }
+
+    public Car(String model, boolean start, double speed) {
+        this.model = model;
+        this.start = start;
+        this.speed = speed;
+    }
+}
+
+
+
